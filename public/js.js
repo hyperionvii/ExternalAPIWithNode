b9d2a773629bb076638f51813d1fff7a8a2e5bd4
Sofia Wicker <sofiazw@gmail.com>
2017-09-22 17:10:35 -0400

    modified append to html so that you can check the weather multiple times

--- public/js.js
+++ public/js.js
@@ -1,15 +1,14 @@
-$("#submit").on("click", function(event) {
-  event.preventDefault();
+jQuery(document).ready(function($) {
 
-  var city = $('#city').val().trim()
-  var state = $('#state').val().trim()
+  $("#submit").on("click", function(event) {
+    event.preventDefault();
 
-  var tempAmount;
-  var rainAmount;
-  var humidityAmount;
+    var city = $('#city').val().trim()
+    var state = $('#state').val().trim()
 
-
-  jQuery(document).ready(function($) {
+    var tempAmount;
+    var rainAmount;
+    var humidityAmount;
 
     $.ajax({
         url : "https://api.wunderground.com/api/92f22e9a41ea4efb/geolookup/conditions/q/" + state + "/" + city + ".json",
@@ -39,34 +38,27 @@ $("#submit").on("click", function(event) {
 
 function temperature(tempAmount) {
   if(tempAmount >= 85) {
-      $("#tempDisplay").append("<img src='images/hotweather.png' style='width:150px; height:165px'/>");
-      $("#tempDisplay").append("<p class='text-center'><strong> It's hot! The temperature is: " + tempAmount + "</p>");
+      $("#tempDisplay").html("<img src='images/hotweather.png' style='width:150px; height:165px'/> <p class='text-center'><strong> It's hot! The temperature is: " + tempAmount + "</p>");
 
   } else if ( tempAmount < 85 && tempAmount >= 65 ) {
-      $("#tempDisplay").html("<p class='text-center'> <strong> Warm! The temperature is: " + tempAmount + "</strong></p>");
-      $("#tempDisplay").append("<img src='images/hotweather.png' alt='temp' style='width:100px; height:100px'/>");
+      $("#tempDisplay").html("<img src='images/hotweather.png' alt='temp' style='width:100px; height:100px'/> <p class='text-center'> <strong> Warm! The temperature is: " + tempAmount + "</strong></p>");
 
   } else if ( tempAmount < 65 && tempAmount >= 55) {
-      $("#tempDisplay").html("<p class='text-center'><strong> Getting chilly...The temperature is: " + tempAmount + "</strong></p>");
-      $("#tempDisplay").append('<img src="images/coldWeather.jpg" alt="temp" style="width:100px; height:100px"/>');
+      $("#tempDisplay").html("<img src='images/coldWeather.jpg' alt='temp' style='width:100px; height:100px'/> <p class='text-center'><strong> Getting chilly...The temperature is: " + tempAmount + "</strong></p>");
 
   } else if ( tempAmount < 55 && tempAmount >= 32) {
-      $("#tempDisplay").html("<p class='text-center'> <strong> Take a jacket. The temperature is: " + tempAmount + "</strong></p>");
-      $("#tempDisplay").append('<img src="images/coldWeather.jpg" alt="temp" style="width:100px; height:100px"/>');
+      $("#tempDisplay").html("<img src='images/coldWeather.jpg' alt='temp' style='width:100px; height:100px'/> <p class='text-center'> <strong> Take a jacket. The temperature is: " + tempAmount + "</strong></p>");
 
   } else if ( tempAmount < 32 ) {
-      $("#tempDisplay").html("<p class='text-center'> <strong> It's freezing! The temperature is: " + tempAmount + "</strong></p>");
-      $("#tempDisplay").append('<img src="images/coldWeather.jpg" alt="temp" style="width:100px; height:100px"/>');
+      $("#tempDisplay").html("<img src='images/coldWeather.jpg' alt='temp' style='width:100px; height:100px'/> <p class='text-center'> <strong> It's freezing! The temperature is: " + tempAmount + "</strong></p>");
   }
 };
 
 function rain(rainAmount) {
   if(rainAmount >= 0 && rainAmount < 1) {
-      $("#rainDisplay").append("<img src='images/no-rain.jpg' style='width:150px; height:165px'/>");
-      $("#rainDisplay").append("<p class='text-center'><strong> No rain! </strong></p>");
+      $("#rainDisplay").html("<img src='images/no-rain.jpg' style='width:150px; height:165px'/><p class='text-center'><strong> No rain! </strong></p>");
   } else if (rainAmount > 1) {
-      $("#rainDisplay").append("<img src='images/umbrella.jpg' style='width:150px; height:165px'/>");
-      $("#rainDisplay").append("<p class='text-center'><strong> It's raining...</strong></p>");
+      $("#rainDisplay").html("<img src='images/umbrella.jpg' style='width:150px; height:165px'/> <p class='text-center'><strong> It's raining...</strong></p>");
   }
 };
 
