bdbbb09ad7e50408958042b5f0b7efee3651a1c4
Sofia Wicker <sofiazw@gmail.com>
2017-09-22 17:17:45 -0400

    update

--- public/js.js
+++ public/js.js
@@ -41,29 +41,31 @@ function temperature(tempAmount) {
       $("#tempDisplay").html("<img src='images/hotweather.png' style='width:150px; height:165px'/> <p class='text-center'><strong> It's hot! The temperature is: " + tempAmount + "</p>");
 
   } else if ( tempAmount < 85 && tempAmount >= 65 ) {
-      $("#tempDisplay").html("<img src='images/hotweather.png' alt='temp' style='width:100px; height:100px'/> <p class='text-center'> <strong> Warm! The temperature is: " + tempAmount + "</strong></p>");
+      $("#tempDisplay").html("<img src='images/hotweather.png' alt='temp' style='width:150px; height:165px'/> <p class='text-center'> <strong> Warm! The temperature is: " + tempAmount + "</strong></p>");
 
   } else if ( tempAmount < 65 && tempAmount >= 55) {
-      $("#tempDisplay").html("<img src='images/coldWeather.jpg' alt='temp' style='width:100px; height:100px'/> <p class='text-center'><strong> Getting chilly...The temperature is: " + tempAmount + "</strong></p>");
+      $("#tempDisplay").html("<img src='images/coldWeather.jpg' alt='temp' style='width:150px; height:165px'/> <p class='text-center'><strong> Getting chilly...The temperature is: " + tempAmount + "</strong></p>");
 
   } else if ( tempAmount < 55 && tempAmount >= 32) {
-      $("#tempDisplay").html("<img src='images/coldWeather.jpg' alt='temp' style='width:100px; height:100px'/> <p class='text-center'> <strong> Take a jacket. The temperature is: " + tempAmount + "</strong></p>");
+      $("#tempDisplay").html("<img src='images/coldWeather.jpg' alt='temp' style='width:150px; height:165px'/> <p class='text-center'> <strong> Take a jacket. The temperature is: " + tempAmount + "</strong></p>");
 
   } else if ( tempAmount < 32 ) {
-      $("#tempDisplay").html("<img src='images/coldWeather.jpg' alt='temp' style='width:100px; height:100px'/> <p class='text-center'> <strong> It's freezing! The temperature is: " + tempAmount + "</strong></p>");
+      $("#tempDisplay").html("<img src='images/coldWeather.jpg' alt='temp' style='width:150px; height:165px'/> <p class='text-center'> <strong> It's freezing! The temperature is: " + tempAmount + "</strong></p>");
   }
 };
 
 function rain(rainAmount) {
-  if(rainAmount >= 0 && rainAmount < 1) {
+  if(rainAmount <= .5) {
       $("#rainDisplay").html("<img src='images/no-rain.jpg' style='width:150px; height:165px'/><p class='text-center'><strong> No rain! </strong></p>");
-  } else if (rainAmount > 1) {
+  } else if (rainAmount > .5) {
       $("#rainDisplay").html("<img src='images/umbrella.jpg' style='width:150px; height:165px'/> <p class='text-center'><strong> It's raining...</strong></p>");
   }
 };
 
 function humidity(humidityAm) {
-  if(humidityAm > 50) {
+  if(humidityAm >= 50) {
     $("#humidityDisplay").html("<img src='images/humid.jpg' style='width:150px; height:165px'/>");
+  } else if (humidityAm < 50) {
+    $("#humidityDisplay").html(" ")
   }
 }
