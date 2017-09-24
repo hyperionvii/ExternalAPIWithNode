2178df90e3f4cfb207ed0d19cf16ac75e7903de4
Sofia Wicker <sofiazw@gmail.com>
2017-09-24 12:15:29 -0400

    debugging

--- config/config.js
+++ config/config.js
@@ -1,10 +1,11 @@
 var config = {};
-//development
+
 var env = process.env.NODE_ENV || 'development';
+
 if(env === 'development'){
-  config = require('./env/development');
+  config = require('./env/envdev.js');
 }else if(env === 'production'){
-  config = require('./env/production');
+  config = require('./env/envproduction.js');
 }
 
 module.exports = config;
